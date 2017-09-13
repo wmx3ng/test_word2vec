ae9ba5e1f1a044ca77a13d80995f8becf2e391c1
wmx <wmx3ng@gmail.com>
2017-09-13 19:55:21 +0800

    add test file

---
+++ word2vec/test_variable_length.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 9/13/17 7:55 PM
+@Author  : wong
+@E-Mail  : wmx3ng@gmail.com
+@File    : test_variable_length.py
+@Software: PyCharm
+@Description:
+"""
+
